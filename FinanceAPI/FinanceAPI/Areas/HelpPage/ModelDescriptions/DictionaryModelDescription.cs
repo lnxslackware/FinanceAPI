cf607908ad7e45c9b07ae6db86f003f2fac461bc
Pavel Hristov <pavelh@abv.bg>
2014-09-12 00:02:35 +0300

    Initial commit

---
+++ FinanceAPI/FinanceAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FinanceAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FinanceAPI/FinanceAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FinanceAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
