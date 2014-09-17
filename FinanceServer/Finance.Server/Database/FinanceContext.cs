c43dd4193613b16504982345857ae8a7e8a33470
Pavel Hristov <pavelh@abv.bg>
2014-09-17 17:13:31 +0300

    Created DbContext

---
+++ FinanceServer/Finance.Server/Database/FinanceContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Finance.Server.Models;
+
+namespace Finance.Server.Database
+{
+    public class FinanceContext : DbContext
+    {
+        public FinanceContext()
+            : base("name=Finance")
+        {
+
+        }
+
+        IDbSet<Account> Accounts { get; set; }
+
+        IDbSet<Stock> Stocks { get; set; }
+
+        IDbSet<Order> Orders { get; set; }
+    }
+}
