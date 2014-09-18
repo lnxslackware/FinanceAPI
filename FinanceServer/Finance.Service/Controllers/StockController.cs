dde4a100dc091102aacaf0ad8180cb397e10eb75
kizisoft <kizisoft@gmail.com>
2014-09-18 22:13:19 +0300

    Order and Stock controlers modifyed to use unity of work

--- FinanceServer/Finance.Service/Controllers/StockController.cs
+++ FinanceServer/Finance.Service/Controllers/StockController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Finance.Data;
+using Finance.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,78 @@ namespace Finance.Service.Controllers
 {
     public class StockController : ApiController
     {
+        private IFinanceData data;
+
+        public StockController()
+            : this(new FinanceData())
+        {
+        }
+
+        public StockController(IFinanceData data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllStocks()
+        {
+            var allStocks = this.data.Stocks.GetAll();
+            return Ok(allStocks);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetStock(int id)
+        {
+            var stockToShow = this.data.Stocks.Get(id);
+            return Ok(stockToShow);
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddStock(Stock stock)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (stock == null)
+            {
+                return BadRequest("You must provide an entry of type 'Stock', 'NULL' provided.");
+            }
+
+            this.data.Stocks.Add(stock);
+            this.data.SaveChanges();
+            var addedStockId = this.data.Stocks.GetAll().FirstOrDefault(s => s.Name == stock.Name);
+            return Ok(addedStockId);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateStock(int id, Stock stock)
+        {
+            Stock stockToUpdate = this.data.Stocks.Get(id);
+            if (stockToUpdate == null)
+            {
+                return BadRequest(string.Format("Failed to update stock. No stock with id {0} found.", id));
+            }
+
+            stockToUpdate.Name = stock.Name;
+            this.data.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteStock(int id)
+        {
+            var stockToDelete = this.data.Stocks.Get(id);
+            if (stockToDelete == null)
+            {
+                return BadRequest(string.Format("No stock with id {0} found to be deleted", id));
+            }
+
+            this.data.Stocks.Delete(stockToDelete);
+            this.data.SaveChanges();
+
+            return Ok(stockToDelete);
+        }
     }
 }
