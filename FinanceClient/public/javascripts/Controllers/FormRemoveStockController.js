b11e5bf8d3754d8fbdc0c73cd163cd987e3a0902
Pavel Hristov <pavelh@abv.bg>
2014-09-19 16:35:01 +0300

    Client

---
+++ FinanceClient/public/javascripts/Controllers/FormRemoveStockController.js
@@ -0,0 +1,19 @@
+app.controller('FormEditStockController', ['$scope', '$http', function ($scope, $http) {
+    $scope.stock = {};
+
+    var postSuccess = function () {
+        $scope.stock = {};
+        $scope.formRemoveStock.$setPristine();
+    };
+
+    var dataBaseError = function () {
+        console.log("DATABASE ERROR.");
+    };
+
+    $scope.updateAccount = function () {
+        $http.put("http://financeapi.apphb.com/api/Stock", JSON.stringify($scope.stock)).
+            success(postSuccess).
+            error(dataBaseError);
+
+    };
+}]);
--- FinanceClient/public/javascripts/main.js
+++ FinanceClient/public/javascripts/main.js
@@ -5,5 +5,7 @@ app.config(['$routeProvider', function ($routeProvider) {
         .when('/remove', { templateUrl: '/partials/remove.html', controller: "FormRemoveAccountController"})
         .when('/add', { templateUrl: '/partials/add.html', controller: "MainController"})
         .when('/stock-add',{templateUrl: '/partials/stock-add.html', controller: "FormAddStockController"})
+        .when('/stock-edit',{templateUrl: '/partials/stock-edit.html', controller: "FormEditStockController"})
+        .when('/stock-remove',{templateUrl: '/partials/stock-remove.html', controller: "FormRemoveStockController"})
         .otherwise({redirectTo: '/'});
 }]);
